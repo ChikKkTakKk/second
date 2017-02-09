aed6cc447a65b23564483c2d3d65bb50b42dfad4
ChikKkTakKk <pva.vapr@mail.ru>
2017-02-09 10:42:06 +0300

    1

---
+++ app.js
@@ -0,0 +1,5 @@
+var greeting = require("./greeting");
+global.name = "Vlad";
+ 
+console.log(date);
+console.log(greeting.getMessage());
---
+++ app3.js
@@ -0,0 +1,4 @@
+var os = require("os");
+var userName = os.userInfo().username;
+
+console.log(userName);
