a6624fd2d865f9869e40d0482ec9dbbfb5eb5fc5
ChikKkTakKk <pva.vapr@mail.ru>
2017-02-09 12:06:44 +0300

    vetka_update

--- app3.js
+++ app3.js
@@ -1,4 +1,8 @@
 var os = require("os");
 var userName = os.userInfo().username;
-
+function Person(name, sername){
+	console.log(name + " " + sername + " " + age);
+}
+var user1 = "Петя"
+Person.name = user1; 
 console.log(userName);
